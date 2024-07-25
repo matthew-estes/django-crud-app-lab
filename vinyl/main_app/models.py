d92f66b605d20c607b4019e485762724a3d21da1
matthew-estes <matthew.estes19@gmail.com>
2024-07-25 17:02:03 -0400

    implement and test home view

--- vinyl/main_app/models.py
+++ vinyl/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    release_year = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    cover_image_url = models.URLField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title} by {self.artist}"
+
