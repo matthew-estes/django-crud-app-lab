dfdffa4b188d24aa20d8a619f021263e3827a4b9
matthew-estes <matthew.estes19@gmail.com>
2024-07-26 10:47:51 -0400

    create a new model for songs

--- vinyl/main_app/models.py
+++ vinyl/main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Album(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
@@ -10,8 +11,19 @@ class Album(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
-    
+
     def get_absolute_url(self):
-         return reverse('album-detail', kwargs={'album_id': self.id})
+        return reverse("album-detail", kwargs={"album_id": self.id})
 
 
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    album = models.ForeignKey(Album, related_name="songs", on_delete=models.CASCADE)
+    duration = models.DurationField()
+    track_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} from {self.album.title}"
+
+    def get_absolute_url(self):
+        return reverse("song-detail", kwargs={"song_id": self.id})
